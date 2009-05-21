3727b619bfbf18a65c8e6412c724520cda85eef7
claytonw@mit.edu <claytonw@mit.edu@1b6154ff-f548-0410-bdff-7fc7a0f7d33b>
2009-05-21 22:19:09 +0000

    This is the new version of the Wockets phone software and Firmware. The bulk of the code is under the Wockets directory. The code is a much cleaner version than what we had before, making it much simpler for the programmer to understand and extend the code.

---
+++ trunk/Firmware/v1/default/dep/wockets_firmware_v1.o.d
@@ -0,0 +1,59 @@
+wockets_firmware_v1.o: ../wockets_firmware_v1.c ../avr_general.h \
+  ../mcu_atmega324p.h ../acc_mma7260qt.h ../bt_rn41.h ../crc.h \
+  ../encoder.h c:/winavr-20090313/lib/gcc/../../avr/include/util/delay.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/stdint.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/util/delay_basic.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/stdlib.h \
+  c:\winavr-20090313\bin\../lib/gcc/avr/4.3.2/include/stddef.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/io.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/iom324.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/iomxx4.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/lock.h
+
+../avr_general.h:
+
+../mcu_atmega324p.h:
+
+../acc_mma7260qt.h:
+
+../bt_rn41.h:
+
+../crc.h:
+
+../encoder.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/util/delay.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/inttypes.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/stdint.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/util/delay_basic.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/stdlib.h:
+
+c:\winavr-20090313\bin\../lib/gcc/avr/4.3.2/include/stddef.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/io.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/iom324.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/iomxx4.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/common.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/version.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/lock.h:
