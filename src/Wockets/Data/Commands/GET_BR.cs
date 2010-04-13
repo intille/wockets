84e9920a12fe055f9fabe2b38bbcbdf6eca8477b
albinali <albinali@mit.edu>
2010-04-13 00:07:02 +0000

    Adding some missing files

---
+++ src/Wockets/Data/Commands/GET_BR.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wockets.Data.Commands
+{
+    public class GET_BR
+    {
+
+        public GET_BR()
+        {
+            this.cmd = new byte[] { (byte)'G', (byte)'U', (byte)13 };
+            this.type = CommandTypes.GET_BAUD_RATE;
+        }
+    }
+}
---
+++ src/Wockets/Data/Responses/CommandModeEnterResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wockets.Data.Responses
+{
+    public sealed class CommandModeEnterResponse: Response
+    {
+
+
+        public CommandModeEnterResponse(int id):base(0,SensorDataType.COMMAND_MODE_ENTERED,(byte)id)
+        {
+        }
+
+    }
+}
