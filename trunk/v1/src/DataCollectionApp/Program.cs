47b6204bc9be47713e49c088124fb1264105916b
albinali@mit.edu <albinali@mit.edu@1b6154ff-f548-0410-bdff-7fc7a0f7d33b>
2010-04-29 16:26:15 +0000

    Adding Data Collection Appa

--- trunk/v1/src/DataCollectionApp/Program.cs
+++ trunk/v1/src/DataCollectionApp/Program.cs
@@ -6,7 +6,13 @@ using System.Threading;
 using System.IO;
 using Wockets;
 using Wockets.Utils;
+using Wockets.Utils.IPC;
 using Wockets.Utils.network;
+using Wockets.Data.Configuration;
+using Wockets.Decoders.Accelerometers;
+using Wockets.Utils.IPC.Queues;
+using Wockets.Receivers;
+using Microsoft.Win32;
 
 namespace DataCollectionApp
 {
@@ -14,8 +20,11 @@ namespace DataCollectionApp
     {
         static Thread interfaceActivityThread;
         static Thread dataCollectionThread;
+        static Thread terminationThread;
         static bool connecting = false;
         static WocketsController wc;
+        static int secondsToConnect = 0;
+        static bool[] countSeconds = null;
         /// <summary>
         /// This application is responsible for collecting data from the wockets, timestamping it and saving it
         /// It can run in 2 modes: - shared memory mode that allows multiple applications to access its buffers
@@ -24,6 +33,11 @@ namespace DataCollectionApp
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+
+           /* RegistryKey rk = Registry.LocalMachine.OpenSubKey("System\\CurrentControlSet\\Control\\Power\\State\\Suspend", true);
+            rk.SetValue("wav1:", 0, RegistryValueKind.DWord);
+            rk.Close();*/
+
             System.IO.DirectoryInfo di = new System.IO.DirectoryInfo("\\");
             System.IO.FileSystemInfo[] fsi = di.GetFileSystemInfos();
             string firstCard = "";
@@ -49,37 +63,113 @@ namespace DataCollectionApp
                     break;
                 }
             }
+            /*
+            try
+            {
+                Directory.Delete(firstCard + "\\Wockets");
+            }
+            catch
+            {
+            }
 
-            string storageDirectory = firstCard + "\\Wockets\\Session" + DateTime.Now.Month + "-" + DateTime.Now.Day + "-" + DateTime.Now.Hour + "-" + DateTime.Now.Minute + "-" + DateTime.Now.Second;
+            try
+            {               
+                File.Delete("test.csv");
+            }
+            catch
+            {
+            }
+            */
+     
+            string storageDirectory = firstCard + "\\Wockets\\Session" + DateTime.Now.Month + "-" + DateTime.Now.Day + "-" + DateTime.Now.Hour + "-" + DateTime.Now.Minute + "-" + DateTime.Now.Second;           
             connecting = true;
             Logger.InitLogger(storageDirectory);
+
+            WocketsConfiguration configuration = new WocketsConfiguration();
+            configuration.FromXML("\\Program Files\\wockets\\NeededFiles\\Master\\Configuration.xml");
+            CurrentWockets._Configuration = configuration;            
             wc = new WocketsController("", "", "");
+            CurrentWockets._Controller = wc;
             wc._storageDirectory = storageDirectory;
             wc.FromXML("\\Program Files\\wockets\\NeededFiles\\SensorConfigurations\\SensorDataFahd.xml");
+            if (!File.Exists("test.csv"))
+            {
+
+                TextWriter tw = new StreamWriter("test.csv", true);
+                tw.WriteLine("Index,");
+                tw.Close();
+            }
+            countSeconds = new bool[wc._Sensors.Count];
             for (int i = 0; (i < wc._Sensors.Count); i++)
+            {
+                wc._Sensors[i]._Loaded = true;
+                wc._Sensors[i]._Flush = true;
                 wc._Sensors[i]._RootStorageDirectory = storageDirectory + "\\data\\raw\\PLFormat\\";
+                countSeconds[i] = false;
+            }
             wc._Bursty = true;
             wc.Initialize();
             //default is local memory
             interfaceActivityThread = new Thread(new ThreadStart(InterfaceActivityTracker));
             interfaceActivityThread.Priority = ThreadPriority.Highest;
             dataCollectionThread = new Thread(new ThreadStart(DataCollection));
+            terminationThread = new Thread(new ThreadStart(TerminateWockets));
+            terminationThread.Start();
             interfaceActivityThread.Start();
             dataCollectionThread.Start();
+
+
+            activitySignal = OpenEvent(EVENT_ALL_ACCESS, false, ActiveName);
+            if (activitySignal == IntPtr.Zero)
+            {
+                // Can't open the events -- this device probably doesn't define them
+                return;
+            }
+            activityListenerThread = new Thread(activityListener);
+            activityListenerThread.Start();
             dataCollectionThread.Join();
 
+        }
+        static void TerminateWockets()
+        {
+            NamedEvents namedEvent = new NamedEvents();
+            namedEvent.Receive("TerminateWockets");
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+
+        private static int EVENT_ALL_ACCESS = 0x1F0003;
+        private static String ActiveName = "PowerManager/UserActivity_Active";
+        private static IntPtr activitySignal;
+        private static Thread activityListenerThread;
+        [DllImport("coredll.dll")]
+        private static extern IntPtr OpenEvent(int desiredAccess, bool inheritHandle, string name);
+
+        [DllImport("coredll.dll")]
+        private static extern int WaitForSingleObject(IntPtr handle, int milliseconds);
+        private static DateTime lastActivity = DateTime.Now;
+        private static void activityListener()
+        {
+            while (true)
+            {
+                // waiting for activity signal, will block the thread, works globally.
+                WaitForSingleObject(activitySignal, int.MaxValue);    
+                lastActivity = DateTime.Now;
+                Thread.Sleep(5000);
+            }
         }
 
         static void InterfaceActivityTracker()
         {
 
             int k = 0;
+            P2PMessageQueue mQue = new P2PMessageQueue(false,"WocketStatistics");
+            int expectedPackets = 0;
             int[] dataSavedSeconds = new int[wc._Sensors.Count];
-            bool[] countSeconds = new bool[wc._Sensors.Count];
+            
             for (int i = 0; (i < wc._Sensors.Count); i++)
             {
                 dataSavedSeconds[i] = 0;
-                countSeconds[i] = false;
+
             }
             while (true)
             {
@@ -88,11 +178,98 @@ namespace DataCollectionApp
                     SystemIdleTimerReset();
                     if ((wc != null) && (wc._Sensors.Count > 0))
                     {
-                        bool dataReceived = true;
+                        //Check 2 things, num of connection failures
+                        // check if data received is > 0
+                        // if num connections >2, ready to pause
+                        // if data received is >0, ready to pause within 2 seconds.
+
+
+                        
+                        bool receivedSomeData = true;
+                        bool receivedFullData = true;
                         for (int i = 0; (i < wc._Sensors.Count); i++)
                         {
-                            //wc._Sensors[i].Save();
-                            if (wc._Sensors[i].Packets < 2400)
+                            //halt, if either 1 successful connection was made
+                            // or any data was received
+                            // or 3 or more reconnections were made
+                            if ((((RFCOMMReceiver)wc._Receivers[i])._SuccessfulConnections == 0) &&
+                                (wc._Sensors[i]._ReceivedPackets == 0) &&
+                                (((RFCOMMReceiver)wc._Receivers[i])._Reconnections <= 3))
+                            {
+                                receivedSomeData = false;
+                                break;
+                            }
+                            else
+                                receivedFullData &= (wc._Sensors[i]._ReceivedPackets == ((WocketsDecoder)wc._Decoders[i])._ExpectedPacketCount);
+                        }
+
+                        if (receivedSomeData)
+                        {
+                            // if didnt get full data, sleep for 2 seconds
+                            if (!receivedFullData)
+                                Thread.Sleep(3000);
+
+                            //save whatever we have so far then sleep
+                            connecting = false;
+                            SYSTEM_POWER_STATUS_EX2 bpower = Battery.GetSystemPowerStatus();
+                            string log_line = ++k + "," + DateTime.Now.ToLongTimeString() + "," + bpower.BatteryLifePercent + "," + bpower.BatteryVoltage + "," + bpower.BatteryCurrent + "," + bpower.BatteryTemperature;
+
+                            for (int i = 0; (i < wc._Sensors.Count); i++)
+                            {
+                                log_line += "," + wc._Sensors[i]._ReceivedPackets + "," + ((RFCOMMReceiver)wc._Receivers[i])._SuccessfulConnections + "," + ((RFCOMMReceiver)wc._Receivers[i])._Reconnections + "," + ((RFCOMMReceiver)wc._Receivers[i])._ConnectionTime
+                                dataSavedSeconds[i] = 0;
+                                countSeconds[i] = false;
+                                ((WocketsDecoder)wc._Decoders[i])._ExpectedPacketCount = 0;
+                            }
+
+                            wc._Polling = false;
+
+                            //shutting down BT here causes a strange delay on wakeup.
+                            while (true)
+                            {
+                                try
+                                {
+                                    if (Wockets.Utils.network.NetworkStacks._BluetoothStack.Dispose())
+                                        break;
+                                }
+                                catch
+                                {
+                                }
+                                SystemIdleTimerReset();
+                                Thread.Sleep(1000);
+                            }
+
+                            TextWriter tw = new StreamWriter("test.csv", true);
+                            tw.WriteLine(log_line);
+                            tw.Close();
+
+                            try
+                            {
+                                mQue.Send(new Message(System.Text.Encoding.ASCII.GetBytes(log_line), false));
+                            }
+                            catch
+                            {
+                            }
+
+                            SystemIdleTimerReset();
+                            for (int i = 0; (i < wc._Sensors.Count); i++)
+                                wc._Sensors[i].Save();
+
+
+
+                            Thread.Sleep(1000);
+                            if (DateTime.Now.Subtract(lastActivity).TotalSeconds > 30)
+                                SetSystemPowerState(null, POWER_STATE_SUSPEND, POWER_FORCE);
+                         
+                        }
+
+
+
+                        /*bool dataReceived = true;
+                        for (int i = 0; (i < wc._Sensors.Count); i++)
+                        {
+                          
+                            if ((wc._Sensors[i]._ReceivedPackets ==0) || (wc._Sensors[i]._ReceivedPackets < ((WocketsDecoder)wc._Decoders[i])._ExpectedPacketCount))
                                 dataReceived = false;
 
                             // if connected once start counting seconds   
@@ -109,34 +286,62 @@ namespace DataCollectionApp
                                 timeoutexpired = false;
                         if ((dataReceived) || (timeoutexpired))
                         {
-                        //    NetworkStacks._BluetoothStack.Dispose();                    
-                            TextWriter tw = new StreamWriter("test.csv", true);
-                            SYSTEM_POWER_STATUS_EX2 bpower = Battery.GetSystemPowerStatus();
-                            tw.Write(++k + "," + DateTime.Now.ToLongTimeString() + "," + bpower.BatteryLifePercent + "," + bpower.BatteryVoltage + "," + bpower.BatteryCurrent + "," + bpower.BatteryTemperature);
 
+                            connecting = false;
+                            SYSTEM_POWER_STATUS_EX2 bpower = Battery.GetSystemPowerStatus();
+                            string log_line = ++k + "," + DateTime.Now.ToLongTimeString() + "," + bpower.BatteryLifePercent + "," + bpower.BatteryVoltage + "," + bpower.BatteryCurrent + "," + bpower.BatteryTemperature;
 
                             for (int i = 0; (i < wc._Sensors.Count); i++)
                             {
-                                tw.Write("," + wc._Sensors[i].Packets);
+                                log_line += "," + wc._Sensors[i]._ReceivedPackets + "," + ((WocketsDecoder)wc._Decoders[i])._ExpectedPacketCount;
                                 dataSavedSeconds[i] = 0;
                                 countSeconds[i] = false;
-                               //wc._Sensors[i].Save();
-                               /* wc._Sensors[i].Packets = 0;
-                                wc._Sensors[i].SavedPackets = 0;
-                                wc._Receivers[i]._Status = Wockets.Receivers.ReceiverStatus.Disconnected;*/
+                                ((WocketsDecoder)wc._Decoders[i])._ExpectedPacketCount = 0;                          
+                            } 
+
+                            wc._Polling = false;
+             
+                            //shutting down BT here causes a strange delay on wakeup.
+                            while (true)
+                            {
+                                try
+                                {
+                                    if (Wockets.Utils.network.NetworkStacks._BluetoothStack.Dispose())
+                                        break;
+                                }
+                                catch
+                                {
+                                }
+                                SystemIdleTimerReset();
+                                Thread.Sleep(1000);
                             }
-                            tw.WriteLine();
-                            connecting = false;
-                            wc.Pause();
-                            //wc.Dispose();    
+                                                                                                             
+                            TextWriter tw = new StreamWriter("test.csv", true);
+                            tw.WriteLine(log_line);
                             tw.Close();
                             
-                            SetSystemPowerState(null, POWER_STATE_SUSPEND, POWER_FORCE);
+                            try
+                            {                             
+                                mQue.Send(new Message(System.Text.Encoding.ASCII.GetBytes(log_line), false));
+                            }
+                            catch
+                            {
+                            }
+
+                            SystemIdleTimerReset();
+                             for (int i = 0; (i < wc._Sensors.Count); i++)
+                                wc._Sensors[i].Save();
+
+                        
+                       
+                            Thread.Sleep(1000);
+                            if (DateTime.Now.Subtract(lastActivity).TotalSeconds > 30)
+                               SetSystemPowerState(null, POWER_STATE_SUSPEND, POWER_FORCE);
                          
-                        }
+                        }*/
                     }
                 }
-
+                
                 Thread.Sleep(1000);
             }
         }
@@ -269,10 +474,22 @@ namespace DataCollectionApp
 
 
                 namedEvent.Receive("MyTestEvent" + k);
+                SystemIdleTimerReset();
+                try
+                {
+                    Wockets.Utils.network.NetworkStacks._BluetoothStack.Initialize();
+                }
+                catch
+                {
+                }
+                Thread.Sleep(3000);
                 //PowerPolicyNotify(PPNMessage.PPN_UNATTENDEDMODE, -1);               
                 //SetSystemPowerState(null,POWER_STATE_ON, POWER_FORCE);
+                for (int i = 0; (i < wc._Sensors.Count); i++)           
+                    countSeconds[i] = true;
                 connecting = true;
-                wc.Unpause();
+                //wc.Unpause();
+                wc._Polling = true;
 
                 k++;
                 //on wakeup, attempt to reconnect
