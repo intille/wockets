0c208e5a723fd39ac7b0dc71ba88105fe52c4792
albinali@mit.edu <albinali@mit.edu@1b6154ff-f548-0410-bdff-7fc7a0f7d33b>
2010-05-26 05:23:39 +0000

    Added bilinear Butterworth filtering

--- trunk/src/WocketsWeka/Utils/Filters/ButterWorth.cs
+++ trunk/src/WocketsWeka/Utils/Filters/ButterWorth.cs
@@ -1,295 +1,92 @@
 ﻿using System;
-
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace WocketsWeka.Utils.Filters
 {
-    public class ButterWorth
+    public class Butterworth : BandPass
     {
+        public SamplingRates _SamplingRate;
+        int sr = 0;
+        double[] a=new double[5] { 1.00, -3.7856, 5.3793, -3.4016, 0.8079 };
+        double[] b;
 
+        double[] xv;
+        double[] yv;
 
-        /**********************************************************************
-  binomial_mult - multiplies a series of binomials together and returns
-  the coefficients of the resulting polynomial.
-  
-  The multiplication has the following form:
-  
-  (x+p[0])*(x+p[1])*...*(x+p[n-1])
+        string filename = "BW_BP_SR_";
+        ButterWorthDF bbutter;
 
-  The p[i] coefficients are assumed to be complex and are passed to the 
-  function as a pointer to an array of doubles of length 2n.
-
-  The resulting polynomial has the following form:
-  
-  x^n + a[0]*x^n-1 + a[1]*x^n-2 + ... +a[n-2]*x + a[n-1]
-  
-  The a[i] coefficients can in general be complex but should in most
-  cases turn out to be real. The a[i] coefficients are returned by the
-  function as a pointer to an array of doubles of length 2n. Storage
-  for the array is allocated by the function and should be freed by the
-  calling program when no longer needed.
-  
-  Function arguments:
-  
-  n  -  The number of binomials to multiply
-  p  -  Pointer to an array of doubles where p[2i] (i=0...n-1) is
-        assumed to be the real part of the coefficient of the ith binomial
-        and p[2i+1] is assumed to be the imaginary part. The overall size
-        of the array is then 2n.
-*/
-
-        double[] binomial_mult(int n, double[] p)
-        {
-            int i, j;
-            double[] a;
-
-            a = new double[2 * n];
+        public Butterworth(Frequency lowFrequency, Frequency highFrequency, Order order, SamplingRates sr): 
+            base(lowFrequency,highFrequency,order)
+        {           
             
-            for (i = 0; i < n; ++i)
-            {
-                for (j = i; j > 0; --j)
-                {
-                    a[2 * j] += p[2 * i] * a[2 * (j - 1)] - p[2 * i + 1] * a[2 * (j - 1) + 1];
-                    a[2 * j + 1] += p[2 * i] * a[2 * (j - 1) + 1] + p[2 * i + 1] * a[2 * (j - 1)];
-                }
-                a[0] += p[2 * i];
-                a[1] += p[2 * i + 1];
-            }
-            return (a);
+        
+            _SamplingRate = sr;
+            //a = new double[((int)order) *2 +1];
+            b = new double[ ((int)order) * 2 + 1];
+            filename+= ((int)_SamplingRate)+"_ORDER_"+((int)order) +"_LF_" + ( (((int)lowFrequency)%10 ==0) ? (((int)lowFrequency)/10).ToString(): (((double)lowFrequency)/10 ).ToString("0.0"))+".csv";
+            TextReader tr = new StreamReader(@"C:\Users\albinali\Desktop\FitFriends\Filters\"+filename);
+            string line = "";
+            int skip = ((((int)highFrequency) - ((int)lowFrequency)) * 2);
+            while (skip-- != 0)
+                tr.ReadLine();
+            string[] tokens = tr.ReadLine().Split(new char[1] { ',' });
+            //for (int i = 0; (i < tokens.Length); i++)
+              //  a[i] = Convert.ToDouble(tokens[i]);
+            tokens = tr.ReadLine().Split(new char[1] { ',' });
+            for (int i = 0; (i < tokens.Length); i++)
+                b[i] = Convert.ToDouble(tokens[i]);
+
+            bbutter = new ButterWorthDF();
+            int[] vals = bbutter.ccof_bwbp(((int)order));
+
+            for (int i = 0; (i < b.Length); i++)
+                b[i] = vals[i];
+
+
+            tr.Close();
+            xv = new double[((int)order)*2 + 1];
+            yv = new double[((int)order)*2 + 1];
         }
 
 
-        /**********************************************************************
-          trinomial_mult - multiplies a series of trinomials together and returns
-          the coefficients of the resulting polynomial.
-  
-          The multiplication has the following form:
 
-          (x^2 + b[0]x + c[0])*(x^2 + b[1]x + c[1])*...*(x^2 + b[n-1]x + c[n-1])
-
-          The b[i] and c[i] coefficients are assumed to be complex and are passed
-          to the function as a pointers to arrays of doubles of length 2n. The real
-          part of the coefficients are stored in the even numbered elements of the
-          array and the imaginary parts are stored in the odd numbered elements.
-
-          The resulting polynomial has the following form:
-  
-          x^2n + a[0]*x^2n-1 + a[1]*x^2n-2 + ... +a[2n-2]*x + a[2n-1]
-  
-          The a[i] coefficients can in general be complex but should in most cases
-          turn out to be real. The a[i] coefficients are returned by the function as
-          a pointer to an array of doubles of length 4n. The real and imaginary
-          parts are stored, respectively, in the even and odd elements of the array.
-          Storage for the array is allocated by the function and should be freed by
-          the calling program when no longer needed.
-  
-          Function arguments:
-  
-          n  -  The number of trinomials to multiply
-          b  -  Pointer to an array of doubles of length 2n.
-          c  -  Pointer to an array of doubles of length 2n.
-        */
-
-        double[] trinomial_mult(int n, double[] b, double[] c)
+        public double[] Filter(double[] data)
         {
-            int i, j;
-            double[] a;
 
-            a = new double[4 * n];
-
-            a[2] = c[0];
-            a[3] = c[1];
-            a[0] = b[0];
-            a[1] = b[1];
-
-            for (i = 1; i < n; ++i)
+            double[] filtered = new double[data.Length];
+                     
+            for (int i = 0; (i < data.Length); i++)
             {
-                a[2 * (2 * i + 1)] += c[2 * i] * a[2 * (2 * i - 1)] - c[2 * i + 1] * a[2 * (2 * i - 1) + 1];
-                a[2 * (2 * i + 1) + 1] += c[2 * i] * a[2 * (2 * i - 1) + 1] + c[2 * i + 1] * a[2 * (2 * i - 1)];
+                int j = 0;
+                for (; (j < xv.Length-1); j++)
+                    xv[j] = xv[j + 1];
+                xv[j] = data[i];
+                //xv[0] = xv[1]; xv[1] = xv[2]; xv[2] = xv[3]; xv[3] = xv[4];
+                //xv[4] = data[i];
 
-                for (j = 2 * i; j > 1; --j)
-                {
-                    a[2 * j] += b[2 * i] * a[2 * (j - 1)] - b[2 * i + 1] * a[2 * (j - 1) + 1] +
-                    c[2 * i] * a[2 * (j - 2)] - c[2 * i + 1] * a[2 * (j - 2) + 1];
-                    a[2 * j + 1] += b[2 * i] * a[2 * (j - 1) + 1] + b[2 * i + 1] * a[2 * (j - 1)] +
-                    c[2 * i] * a[2 * (j - 2) + 1] + c[2 * i + 1] * a[2 * (j - 2)];
-                }
+                j = 0;
+                for (; (j < yv.Length - 1); j++)
+                    yv[j] = yv[j + 1];
+                yv[j] = 0;// data[i];
 
-                a[2] += b[2 * i] * a[0] - b[2 * i + 1] * a[1] + c[2 * i];
-                a[3] += b[2 * i] * a[1] + b[2 * i + 1] * a[0] + c[2 * i + 1];
-                a[0] += b[2 * i];
-                a[1] += b[2 * i + 1];
+               // yv[0] = yv[1]; yv[1] = yv[2]; yv[2] = yv[3]; yv[3] = yv[4];
+               // yv[4] = (xv[0] + xv[4]) - 2 * xv[2] -
+                 //   a[1] * yv[3] - a[2] * yv[2] - a[3] * yv[1] - a[4] * yv[0];
+               
+                for (int k=1; (k < yv.Length); k++)
+                    yv[j] -= a[k] * yv[yv.Length - k -1];
+                for (int k = 0; (k < xv.Length); k++)
+                    yv[j] += b[k] * xv[k];
+
+                filtered[i] = yv[j];
             }
 
-            return (a);
+            
+            return filtered;
         }
-
-        /**********************************************************************
-    dcof_bwbp - calculates the d coefficients for a butterworth bandpass 
-        filter. The coefficients are returned as an array of doubles.
-
-    */
-
-        public double[] dcof_bwbp(int n, double f1f, double f2f)
-        {
-            int k;            // loop variables
-            double theta;     // M_PI * (f2f - f1f) / 2.0
-            double cp;        // cosine of phi
-            double st;        // sine of theta
-            double ct;        // cosine of theta
-            double s2t;       // sine of 2*theta
-            double c2t;       // cosine 0f 2*theta
-            double[] rcof;     // z^-2 coefficients
-            double[] tcof;     // z^-1 coefficients
-            double[] dcof;     // dk coefficients
-            double parg;      // pole angle
-            double sparg;     // sine of pole angle
-            double cparg;     // cosine of pole angle
-            double a;         // workspace variables
-
-            cp = Math.Cos(Math.PI * (f2f + f1f) / 2.0);
-            theta = Math.PI * (f2f - f1f) / 2.0;
-            st = Math.Sin(theta);
-            ct = Math.Cos(theta);
-            s2t = 2.0 * st * ct;        // sine of 2*theta
-            c2t = 2.0 * ct * ct - 1.0;  // cosine of 2*theta
-
-            rcof = new double[2 * n];
-            tcof = new double[2 * n];
-
-            for (k = 0; k < n; ++k)
-            {
-                parg = Math.PI * (double)(2 * k + 1) / (double)(2 * n);
-                sparg = Math.Sin(parg);
-                cparg = Math.Cos(parg);
-                a = 1.0 + s2t * sparg;
-                rcof[2 * k] = c2t / a;
-                rcof[2 * k + 1] = s2t * cparg / a;
-                tcof[2 * k] = -2.0 * cp * (ct + st * sparg) / a;
-                tcof[2 * k + 1] = -2.0 * cp * st * cparg / a;
-            }
-
-            dcof = trinomial_mult(n, tcof, rcof);
-
-            dcof[1] = dcof[0];
-            dcof[0] = 1.0;
-            for (k = 3; k <= 2 * n; ++k)
-                dcof[k] = dcof[2 * k - 2];
-            return (dcof);
-        }
-
-
-        /**********************************************************************
-  ccof_bwlp - calculates the c coefficients for a butterworth lowpass 
-  filter. The coefficients are returned as an array of integers.
-
-*/
-
-        public int[] ccof_bwlp(int n)
-        {
-            int[] ccof;
-            int m;
-            int i;
-
-            ccof = new int[n + 1];
-
-
-            ccof[0] = 1;
-            ccof[1] = n;
-            m = n / 2;
-            for (i = 2; i <= m; ++i)
-            {
-                ccof[i] = (n - i + 1) * ccof[i - 1] / i;
-                ccof[n - i] = ccof[i];
-            }
-            ccof[n - 1] = n;
-            ccof[n] = 1;
-
-            return (ccof);
-        }
-
-        /**********************************************************************
-          ccof_bwhp - calculates the c coefficients for a butterworth highpass 
-          filter. The coefficients are returned as an array of integers.
-
-        */
-
-        public int[] ccof_bwhp(int n)
-        {
-            int[] ccof;
-            int i;
-
-            ccof = ccof_bwlp(n);
-
-            for (i = 0; i <= n; ++i)
-                if ((i % 2)!=0) ccof[i] = -ccof[i];
-
-            return (ccof);
-        }
-
-        /**********************************************************************
-  ccof_bwbp - calculates the c coefficients for a butterworth bandpass 
-  filter. The coefficients are returned as an array of integers.
-
-*/
-
-        public int[] ccof_bwbp(int n)
-        {
-            int[] tcof;
-            int[] ccof;
-            int i;
-
-            ccof = new int[2 * n + 1];
-
-            tcof = ccof_bwhp(n);
-
-            for (i = 0; i < n; ++i)
-            {
-                ccof[2 * i] = tcof[i];
-                ccof[2 * i + 1] = 0;
-            }
-            ccof[2 * n] = tcof[n];
-
-            return (ccof);
-        }
-
-
-        /**********************************************************************
-  sf_bwbp - calculates the scaling factor for a butterworth bandpass filter.
-  The scaling factor is what the c coefficients must be multiplied by so
-  that the filter response has a maximum value of 1.
-
-*/
-
-        public double sf_bwbp(int n, double f1f, double f2f)
-        {
-            int k;            // loop variables
-            double ctt;       // cotangent of theta
-            double sfr, sfi;  // real and imaginary parts of the scaling factor
-            double parg;      // pole angle
-            double sparg;     // sine of pole angle
-            double cparg;     // cosine of pole angle
-            double a, b, c;   // workspace variables
-
-            ctt = 1.0 / Math.Tan(Math.PI * (f2f - f1f) / 2.0);
-            sfr = 1.0;
-            sfi = 0.0;
-
-            for (k = 0; k < n; ++k)
-            {
-                parg = Math.PI * (double)(2 * k + 1) / (double)(2 * n);
-                sparg = ctt + Math.Sin(parg);
-                cparg = Math.Cos(parg);
-                a = (sfr + sfi) * (sparg - cparg);
-                b = sfr * sparg;
-                c = -sfi * cparg;
-                sfr = b - c;
-                sfi = a - b - c;
-            }
-
-            return (1.0 / sfr);
-        }
-
+      
     }
 }
---
+++ trunk/src/WocketsWeka/Utils/Filters/Filter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WocketsWeka.Utils.Filters
+{
+    public abstract class Filter
+    {
+        public Type _Type;
+        public Order _Order;
+        protected int order;
+
+        public Filter(Type type, Order order)
+        {
+            _Type = type;
+            _Order = order;
+        }
+    }
+}
