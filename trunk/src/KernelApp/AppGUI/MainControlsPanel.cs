79b6c051835ffd287a3d59b0965ffe4460221279
atenea@media.mit.edu <atenea@media.mit.edu@1b6154ff-f548-0410-bdff-7fc7a0f7d33b>
2010-08-18 19:46:31 +0000

    KernelApp Source Code Added to project

---
+++ trunk/src/KernelApp/AppGUI/MainControlsPanel.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+//using WocketsApp.Properties;
+
+
+namespace KernelApp.GUI
+{
+    class MainControlPanel:Panel
+    {
+
+        public ImageButtonClick MainMenuButton;
+        public ImageButtonClick RightButton;
+        public ImageButtonClick LeftButton;
+
+        private int panel_width;
+        private int panel_height;
+        private Color panel_bgcolor = Color.Gainsboro;
+
+
+        //Labels
+        private Label label_left = new Label();
+        private Label label_center = new Label();
+        private Label label_right = new Label();
+
+
+        public MainControlPanel(int width, int height,
+                                 Bitmap leftImage, Bitmap leftImagePressed,
+                                 Bitmap centerImage, Bitmap centerImagePressed,
+                                 Bitmap rightImage, Bitmap rightImagePressed,
+                                 String sleft, String scenter, String sright)
+         {
+         
+            int upper_gap = 15;
+
+            //Wall parameters
+            panel_width = width;
+            panel_height = height;
+
+            this.Size = new Size(panel_width, panel_height);
+            this.Location = new Point(0, 0);
+            this.Visible = true;
+            this.BackColor = panel_bgcolor;
+
+
+            //labels parameters
+            Color label_color = Color.DimGray;
+            Font panel_labels_font = new Font("Tahoma", 8, FontStyle.Regular);
+            int labels_width = 90;
+            int labels_height = 30;
+
+
+            //buttons
+            int button_width = rightImage.Width;
+            int button_height = rightImage.Height;
+            int pos_y = upper_gap + button_height;
+
+            ////Button Prev Button
+            LeftButton = new ImageButtonClick(leftImage, leftImagePressed);
+            LeftButton.Size = new Size(button_width, button_height);
+            LeftButton.Location = new Point(45, upper_gap);
+            LeftButton.Visible = true;
+            this.Controls.Add(LeftButton);
+
+            //Previous Label
+            label_left.Text = sleft;
+            label_left.Font = panel_labels_font;
+            label_left.TextAlign = ContentAlignment.TopCenter;
+            label_left.ForeColor = label_color;
+            label_left.Size = new Size(labels_width, labels_height);
+            label_left.Location = new Point(30, pos_y);
+            this.Controls.Add(label_left);
+
+
+
+            ////Button Main Menu
+            MainMenuButton = new ImageButtonClick( centerImage, centerImagePressed);
+            MainMenuButton.Size = new Size(button_width, button_height);
+            MainMenuButton.Location = new Point(205, upper_gap);
+            MainMenuButton.Visible = true;
+            this.Controls.Add(MainMenuButton);
+            
+            //Main Menu Label
+            label_center.Text = scenter;
+            label_center.Font = panel_labels_font;
+            label_center.TextAlign = ContentAlignment.TopCenter;
+            label_center.ForeColor = label_color;
+            label_center.Size = new Size(labels_width, labels_height);
+            label_center.Location = new Point(190, pos_y);
+            this.Controls.Add(label_center);
+
+
+            ////Button Next Button
+            RightButton = new ImageButtonClick(rightImage, rightImagePressed);
+            RightButton.Size = new Size(button_width, button_height);
+            RightButton.Location = new Point(365, upper_gap);
+            RightButton.Visible = true;
+            this.Controls.Add(RightButton);
+
+            //Next Label
+            label_right.Text = sright;
+            label_right.Font = panel_labels_font;
+            label_right.TextAlign = ContentAlignment.TopCenter;
+            label_right.ForeColor = label_color;
+            label_right.Size= new Size(labels_width, labels_height);
+            label_right.Location = new Point(350, pos_y);
+            this.Controls.Add(label_right);
+
+
+           
+
+        }
+
+
+        
+    }
+}
---
+++ trunk/src/KernelApp/AppGUI/PlottingPanel.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Threading;
+
+namespace KernelApp.GUI
+{
+    class PlottingPanel:Panel
+    {
+        //for double buffering.
+        private Graphics gxOff; //Offscreen graphics
+        private Bitmap m_bmpOffscreen = null;
+        private Pen panelPen;
+        private Color pen_color;
+        private int pen_size = 2;
+
+       
+        //data array
+        public int historyCount = 0;
+        public List<int> shortHistoryData;
+        
+
+
+        public PlottingPanel(int width, int height, Color pcolor, Color pen_color, int pen_size)
+        {
+            
+            this.Size = new Size(width, height);
+            this.BackColor = pcolor;
+            this.Location = new Point(0, 0);
+            this.Visible = true;
+
+            this.pen_color = pen_color;
+            this.pen_size = pen_size;
+            this.panelPen = new Pen(this.pen_color, this.pen_size);
+
+
+            historyCount = width;
+            shortHistoryData = new List<int>(historyCount);
+
+            
+            //Graphics
+            if (m_bmpOffscreen == null) //Bitmap for doublebuffering
+            {
+                m_bmpOffscreen = new Bitmap(this.Size.Width, this.Size.Height);
+            }
+            gxOff = Graphics.FromImage(m_bmpOffscreen);
+
+
+        }
+
+        
+     
+        int MAX_AC = 500;
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            // code to paint plot
+            gxOff.Clear(this.BackColor);
+
+            double raw_act= 0;
+            double actp = 0;
+            int act = 0;
+
+            for (int i = (shortHistoryData.Count - 1); i >= 0; i--)
+            {
+                raw_act =shortHistoryData[shortHistoryData.Count - 1 - i] / 60;
+                actp = raw_act/MAX_AC;
+                act = (int)Math.Floor(actp * this.Height);
+
+                gxOff.DrawLine(panelPen, 
+                               historyCount - i, this.Height, 
+                               historyCount - i, 
+                               this.Height - act);
+            }
+           
+            e.Graphics.DrawImage(m_bmpOffscreen, 0, 0, 
+                                new Rectangle(0, 0, this.Size.Width, 
+                              this.Size.Height), GraphicsUnit.Pixel);
+        }
+
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            //do nothing to avoid flickering
+        }
+
+       
+
+        public void updateData(int newdata)
+        {   
+            if (shortHistoryData.Count == historyCount)
+            {
+                shortHistoryData.RemoveAt(0);
+            }
+
+            shortHistoryData.Add(newdata);
+
+           
+            this.Invalidate();
+        }
+
+
+       
+
+        public void updateData(int[] newData)
+        {
+            DataToAdd batchData = new DataToAdd(newData, shortHistoryData, historyCount, this);
+            Thread addBatchDataThread = new Thread(batchData.startAdding);
+            addBatchDataThread.Start();
+        }
+
+
+        // class for adding data animation
+        internal class DataToAdd
+        {
+            private int[] newData;
+            private List<int> showDataHistory;
+            private PlottingPanel pPanel;
+            private int histCount;
+            
+            // in declaration
+            private delegate void RefreshPlotDelegate();
+
+
+            public DataToAdd(int[] newData, List<int> showDataHistory, int historyCount, PlottingPanel plottingPanel)
+            {
+                this.newData = newData;
+                this.showDataHistory = showDataHistory;
+                this.pPanel = plottingPanel;
+                this.histCount = historyCount;
+            }
+
+            // in internal class
+            private void refreshPlot()
+            {
+                pPanel.Invalidate();
+            }
+
+
+            internal void startAdding()
+            {
+                for (int i = 0; i < newData.Length; i++)
+                {
+
+                    if (showDataHistory.Count == histCount)
+                    {
+                        showDataHistory.Remove(0);
+                    }
+
+                    showDataHistory.Add(newData[i]);
+
+                    // at where you want to call it. 
+                    pPanel.BeginInvoke(new RefreshPlotDelegate(refreshPlot));
+
+                    //pPanel.Invalidate();
+                    Thread.Sleep(20);
+
+                }
+            }
+        }
+
+
+
+
+
+    }
+}
