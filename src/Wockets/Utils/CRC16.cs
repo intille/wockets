5b4d92d177f2507eb1b8487bf684cd8f35fe755c
albinali <albinali@mit.edu>
2010-10-15 02:18:06 +0000

    Adding CRC16

---
+++ src/Wockets/Utils/CRC16.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wockets.Utils
+{
+    public class CRC16
+    {
+
+        public static UInt16 Compute(byte[] data, int len)
+        {
+            UInt16 crc = 0;
+            for (int i = 0; (i < len); i++)
+            {
+                crc ^= (UInt16)(data[i] << 8);
+
+                for (int j = 0; j < 8; ++j)
+                {
+                    if (((UInt16)(crc & 0x8000)) != 0)
+                        crc = (UInt16)((UInt16)(crc << 1) ^ 0x1021);
+                    else
+                        crc = (UInt16)(crc << 1);
+                }
+            }
+            return crc;
+        }
+    }
+}
