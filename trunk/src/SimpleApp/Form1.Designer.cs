360e39f315b249e72b865cf5c026f28978d306c7
atenea@media.mit.edu <atenea@media.mit.edu@1b6154ff-f548-0410-bdff-7fc7a0f7d33b>
2010-10-21 19:40:35 +0000

    Simple Sample Application Added to PPC solution.

---
+++ trunk/src/SimpleApp/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace KernelApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer();
+            this.button_minimize = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 72);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 269);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Quit";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(16, 83);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(206, 180);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button_minimize
+            // 
+            this.button_minimize.Location = new System.Drawing.Point(150, 269);
+            this.button_minimize.Name = "button_minimize";
+            this.button_minimize.Size = new System.Drawing.Size(72, 35);
+            this.button_minimize.TabIndex = 2;
+            this.button_minimize.Text = "Minimize";
+            this.button_minimize.Click += new System.EventHandler(this.button_minimize_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(240, 320);
+            this.Controls.Add(this.button_minimize);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button_minimize;
+
+
+        #region commented
+        //private System.Windows.Forms.MenuItem menuItem1;
+        //private System.Windows.Forms.MenuItem menuItem3;
+        //private System.Windows.Forms.MenuItem menuItem2;
+        //private System.Windows.Forms.Button button1;
+        //private System.Windows.Forms.Label label1;
+        //private System.Windows.Forms.Label status;
+        //private System.Windows.Forms.MenuItem menuItem4;
+        #endregion commented
+
+    }
+}
+
