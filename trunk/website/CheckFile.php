012bfbd728fcc30fa12cb41db4ba2e1cf1587ba7
albinali@mit.edu <albinali@mit.edu@1b6154ff-f548-0410-bdff-7fc7a0f7d33b>
2010-12-29 05:54:19 +0000

    Updated server side file check

--- trunk/website/CheckFile.php
+++ trunk/website/CheckFile.php
@@ -21,16 +21,32 @@
 
 	mysql_select_db($database_Wockets, $Wockets);
 	
-	$query_Recordset1 = sprintf("SELECT * FROM FILE_UPLOAD WHERE imei ='".$imei."' AND filename='".$filename."' AND relative_path='".$relative_path."'");
-	$Recordset1 = mysql_query($query_Recordset1, $Wockets) or die(mysql_error());
-	$row_Recordset1 = mysql_fetch_assoc($Recordset1);
-	$totalRows_Recordset1 = mysql_num_rows($Recordset1);
 
-	$absfilename=getcwd()."/WocketsData/".$relative_path."/".$filename;	
-	if (($totalRows_Recordset1==1) && ($filename!="")&&(file_exists ($absfilename)))
-	{	
-		$md5=md5_file($absfilename);
-		echo "true,".$md5.",".$row_Recordset1['sender_date'].",".$row_Recordset1['server_date'];		
+	//echo $query_Recordset1;
+	$query_Recordset2 = sprintf("SELECT * FROM PHONES,PARTICIPANTS_PHONE WHERE PHONES.imei ='".$imei."' AND PHONES.id=PARTICIPANTS_PHONE.phone_id");
+	$Recordset2 = mysql_query($query_Recordset2, $Wockets) or die(mysql_error());
+	$row_Recordset2 = mysql_fetch_assoc($Recordset2);
+	$totalRows_Recordset2 = mysql_num_rows($Recordset2);
+
+	if ($totalRows_Recordset2==1) 
+	{
+		$participant_id=$row_Recordset2['participant_id'];
+		$relative_path="Subject".$participant_id. "/" . $relative_path;
+		$absfilename=getcwd()."/WocketsData/".$relative_path."/".$filename;	 
+	 	$query_Recordset1 = sprintf("SELECT * FROM FILE_UPLOAD WHERE imei ='".$imei."' AND filename='".$filename."' AND relative_path='".$relative_path."'");
+		$Recordset1 = mysql_query($query_Recordset1, $Wockets) or die(mysql_error());
+		$row_Recordset1 = mysql_fetch_assoc($Recordset1);
+		$totalRows_Recordset1 = mysql_num_rows($Recordset1); 
+		if (($totalRows_Recordset1==1) && ($filename!="")&&(file_exists ($absfilename)))
+		{	
+			$md5=md5_file($absfilename);
+			echo "true,".$md5.",".$row_Recordset1['sender_date'].",".$row_Recordset1['server_date'];		
+		}else
+			echo "false";		
+			
 	}else
-		echo "false";			
+		echo "false";		
+			
+
+		
 ?>
