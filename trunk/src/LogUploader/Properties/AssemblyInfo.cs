150e3f90f6786337ae56f2eb420683453de50541
albinali@mit.edu <albinali@mit.edu@1b6154ff-f548-0410-bdff-7fc7a0f7d33b>
2010-12-30 06:37:10 +0000

    Added stats logging and sending to the website

--- trunk/src/LogUploader/Properties/AssemblyInfo.cs
+++ trunk/src/LogUploader/Properties/AssemblyInfo.cs
@@ -2,35 +2,32 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CollectData")]
+[assembly: AssemblyTitle("LogUploader")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CollectData")]
+[assembly: AssemblyProduct("LogUploader")]
 [assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("101f3b3a-7cc9-4d82-95ae-e4332d5d893b")]
+[assembly: Guid("c17686c5-7b66-483b-a9a9-4654a31eb98c")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.700.2010.1229")]
+[assembly: AssemblyVersion("1.0.0.0")]
 
-// Below attribute is to suppress FxCop warning "CA2232 : Microsoft.Usage : Add STAThreadAttribute to assembly"
-// as Device app does not support STA thread.
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2232:MarkWindowsFormsEntryPointsWithStaThread")]
