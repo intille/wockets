c35e83b6f984323f65ea08031fc3a052f120e141
albinali <albinali@mit.edu>
2010-12-30 21:46:45 +0000

    Updating code to push activity counts to the web site.

--- src/CollectData/Properties/AssemblyInfo.cs
+++ src/CollectData/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.700.2010.1229")]
+[assembly: AssemblyVersion("1.702.2010.1230")]
 
 // Below attribute is to suppress FxCop warning "CA2232 : Microsoft.Usage : Add STAThreadAttribute to assembly"
 // as Device app does not support STA thread.
