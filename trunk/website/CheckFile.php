8d389e42fae15c3a5f8cc770e2a30d8ea18d8784
albinali@mit.edu <albinali@mit.edu@1b6154ff-f548-0410-bdff-7fc7a0f7d33b>
2011-01-04 04:35:49 +0000

    Updated check file and log status with tests to avoid duplicate entries in the tables

--- trunk/website/CheckFile.php
+++ trunk/website/CheckFile.php
@@ -34,10 +34,12 @@
 		$relative_path="Subject".$participant_id. "/" . $relative_path;
 		$absfilename=getcwd()."/WocketsData/".$relative_path."/".$filename;	 
 	 	$query_Recordset1 = sprintf("SELECT * FROM FILE_UPLOAD WHERE imei ='".$imei."' AND filename='".$filename."' AND relative_path='".$relative_path."'");
+
 		$Recordset1 = mysql_query($query_Recordset1, $Wockets) or die(mysql_error());
 		$row_Recordset1 = mysql_fetch_assoc($Recordset1);
 		$totalRows_Recordset1 = mysql_num_rows($Recordset1); 
-		if (($totalRows_Recordset1==1) && ($filename!="")&&(file_exists ($absfilename)))
+
+		if (($totalRows_Recordset1>=1) && ($filename!="")&&(file_exists ($absfilename)))
 		{	
 			$md5=md5_file($absfilename);
 			echo "true,".$md5.",".$row_Recordset1['sender_date'].",".$row_Recordset1['server_date'];		
