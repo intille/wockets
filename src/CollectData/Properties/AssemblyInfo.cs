f569caeb57c4c193c4bd02a0467a3aa16b5b7c0a
albinali <albinali@mit.edu>
2011-01-09 21:55:48 +0000

    Resolved conflict

--- src/CollectData/Properties/AssemblyInfo.cs
+++ src/CollectData/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.709.2011.12")]
+[assembly: AssemblyVersion("1.718.2011.15")]
 
 // Below attribute is to suppress FxCop warning "CA2232 : Microsoft.Usage : Add STAThreadAttribute to assembly"
 // as Device app does not support STA thread.
