c5865571d4b84c6a35457095afd8b0476d5bb1e0
smotatoledo@gmail.com <smotatoledo@gmail.com@1b6154ff-f548-0410-bdff-7fc7a0f7d33b>
2011-01-23 06:25:22 +0000

    masterlist and sensordata.xml updated to better handle sensor swap.

--- trunk/src/CollectData/Properties/AssemblyInfo.cs
+++ trunk/src/CollectData/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.718.2011.15")]
+[assembly: AssemblyVersion("1.725.2011.119")]
 
 // Below attribute is to suppress FxCop warning "CA2232 : Microsoft.Usage : Add STAThreadAttribute to assembly"
 // as Device app does not support STA thread.
