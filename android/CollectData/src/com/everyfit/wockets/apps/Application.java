dd9ee60639d431de803743d268af1c7adb013d42
albinali <albinali@mit.edu>
2011-02-23 22:57:17 +0000

    Adding some missing files

---
+++ android/CollectData/src/com/everyfit/wockets/apps/Application.java
@@ -0,0 +1,12 @@
+package com.everyfit.wockets.apps;
+
+import android.content.Context;
+
+import com.everyfit.wockets.WocketsController;
+
+public class Application {
+
+	public static WocketsController _Controller=null;
+	public static Context _Context=null;
+	public static Wockets _Wockets=null;
+}
