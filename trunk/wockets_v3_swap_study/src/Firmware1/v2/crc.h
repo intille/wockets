71b911bd7a8061cb44d1f125f9e9a77698a45c3c
StephenIntille@gmail.com <StephenIntille@gmail.com@1b6154ff-f548-0410-bdff-7fc7a0f7d33b>
2011-08-26 15:31:43 +0000

    Major SVN update: Deleting old/backup directories. These are backed up elsewhere now as of August 26, 2011.

--- trunk/wockets_v3_swap_study/src/Firmware1/v2/crc.h
+++
@@ -1,7 +0,0 @@
-#define POLYNOMIAL 0xD8
-#define WIDTH (8 *sizeof(unsigned char))
-#define TOPBIT (1<<(WIDTH-1))
-unsigned char crcTable[256];
-
-void init_crc(void);
-unsigned char crc(unsigned char message[],unsigned char length);
