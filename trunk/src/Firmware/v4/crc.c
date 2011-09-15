3bf22e028471c913193df3466ba8025144bc7927
yogi.res@gmail.com <yogi.res@gmail.com@1b6154ff-f548-0410-bdff-7fc7a0f7d33b>
2011-09-15 02:54:46 +0000

    Commented crc.c, crc.h and wocket.c. Deleted some commented sections of wocket.c.

--- trunk/src/Firmware/v4/crc.c
+++ trunk/src/Firmware/v4/crc.c
@@ -1,32 +1,33 @@
-#define  POLYNOMIAL (0x1070 << 3)
+/* Currently (Sept 2, 2011) the crc.c functions are not being used */
 
+#define  POLYNOMIAL (0x1070 << 3)		// Polynomial CRC 
 
-
-
-  unsigned char ComputeByte(unsigned char inCrc, unsigned char inData)
-  {
-     int i;
-     unsigned short data;
-     data = (unsigned short)(inCrc ^ inData);
-     data <<= 8;
-     for (i = 0; i < 8; i++)
-     {
+/* Function called by the computerCRC8 and CRC16 functions below*/
+unsigned char ComputeByte(unsigned char inCrc, unsigned char inData)
+{
+    int i;
+    unsigned short data;
+	// Xor'ing inCrc with inData
+    data = (unsigned short)(inCrc ^ inData);
+    data <<= 8;
+    for (i = 0; i < 8; i++)
+   	{
         if ((data & 0x8000) != 0)
         	data = (unsigned short)(data ^ POLYNOMIAL);
-        data = (unsigned short)(data << 1);
-     }
-     return (unsigned char)(data >> 8);
-   }
+    	data = (unsigned short)(data << 1);
+    }
+	return (unsigned char)(data >> 8);
+}
 
-   
-  unsigned char ComputeCRC8(unsigned char crc, unsigned char *data, int len)
-  {
-      for (int i = 0; (i < len); i++)
-          crc = ComputeByte(crc, *(data+i));
-       return crc;
-  }
+/* Compute CRC for one byte*/ 
+unsigned char ComputeCRC8(unsigned char crc, unsigned char *data, int len)
+{
+	for (int i = 0; (i < len); i++)
+    	crc = ComputeByte(crc, *(data+i));
+    return crc;
+}
 
-
+/* Compute CRC for two bytes of data or unsigned short*/   
 unsigned short CRC16(unsigned char *buf, int len )
 {
 	unsigned short crc = 0;
