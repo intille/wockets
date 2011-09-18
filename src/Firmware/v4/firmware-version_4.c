b39035242bc3b5e530fcca98ccbb3c9b9baff46d
yogi.res <yogi.res@gmail.com>
2011-09-18 18:15:49 +0000

    Included comments in the firmware-version_4.c

--- src/Firmware/v4/firmware-version_4.c
+++ src/Firmware/v4/firmware-version_4.c
@@ -31,94 +31,120 @@ unsigned char wakeup=0;
 unsigned char sample=0;
 
 
-/*data_unit data1[100];
-data_unit data2[100];
-data_unit data3[100];
-data_unit data4[100];
-data_unit data5[100];
-data_unit data6[100];
-data_unit data7[50];*/
 
-//unsigned short xs[3000];
-//unsigned short ys[3000];
-//unsigned short zs[3000];
-
-data_unit data[750];
-unsigned short acount[AC_BUFFER_SIZE]; //array 960 capacity => If 1 AC per minute = 16h of capacity
+data_unit data[750];					// Structure defined in wocket.h has 16 unsigned char bytes per data_unit
+unsigned short acount[AC_BUFFER_SIZE]; //array 960 capacity => If 1 AC (activity count) per minute = 16h of capacity 
+// acount buffer is used to store activity counts in case the wocket's losses the Blutooth connection (phone)
 unsigned short AC_NUMS=0;
-unsigned short AC_SCALING=24;
+unsigned short AC_SCALING=24; // Scaling factor is based on previous testing and explained below 
+/* 2 bytes or 16 bit activity count is sent to the phone every minute [an integration of 2400 samples of filtered
+raw data (using a moving average filter) where 2400 is 60x40samples]. 16 bit activity count was chosen due to memory
+limitations on the mcu [required to store 1.5 minutes of raw data and 16 hours of activity counts]
+The integration for activity counts will cause the 16 bit number to overflow, based on experimentation with a 
+4g accelerometer with a 10-bit ADC, 24 was determined to be a reasonable scaling factor that allows for the 
+capture of high intensity activity without overflow */
+
+/*Quick calculation: Summary Data or activity counts = 16bits = 2bytes X 960 (16hrs x60) = 1920 bytes; 
+Raw Data= 30 bits X 40 samples = 1200 bits = 150 bytes /second which leads to 9000 bytes/minute 
+Storage of raw data for 1.5 minutes equals 13500  bytes
+
+Total = 1920 +13500= 15420/1024 ~ 15Kbyes
+The MCU has 16K memory and at least 1K of memory is used for locals */
+
 
 unsigned short summaryindex=0;
-unsigned short summary_count=2400;
+unsigned short summary_count=2400;  //One minute worth of activity counts
 
+
+/* Cseq, ci, si, sseq: A circular buffer for the activity counts of size 960 (60 mins x16 hours) is maintained
+on the mcu locally. 
+ci is a pointer that points to the next empty slot. 
+si points to the oldest value in the buffer (i.e. the next value to be sent out or was sent out but no acknowledgment
+received from the phone). 
+The wockets firmware will send activity count data starting from the data pointed by si.
+Cseq and sseq hold the sequence numbers of the entries in ci and si, respectively. The sequence numbers always grow 
+and sseq<=cseq.  
+In some older hardware versions, the wockets used to reset easily, which caused the seq #s to reset and therefore 
+the receiver would get out of sync seeing small seq numbers that were already ACKed. 
+In the code, there is a test done to deal with that situation. */
 unsigned short cseq=0;
 unsigned short sseq=0;
 unsigned short kseq=0;
 unsigned short dseq=0;
 unsigned short ci=0;
 unsigned short si=0;
-unsigned char gotack=0;
 
 
-unsigned short x;
+unsigned char gotack=0;				// When the wocket receives an ACK form the Phone
+
+
+unsigned short x;					// Acceleration values of x, y and z axis
 unsigned short y;
 unsigned short z;
 
-unsigned short battery; 
+unsigned short battery; 			// Contains battery level after sampling the battery 
 
-unsigned short prevx;
-unsigned short prevy;
-unsigned short prevz;
-unsigned char deltasign;
-unsigned char compress=0;
+unsigned short prevx;				// Previous acceleration values x 
+unsigned short prevy;				// Previous acceleration values y
+unsigned short prevz;				// Previous acceleration values z 
+unsigned char deltasign;			/* is a variable that has the information if acc is < 32 in any or all axes;
+ Is set to zero or one based on increment and decrement compared to the previous*/
+unsigned char compress=0;			/* Flag that decides if the data that is to be sent is compressed or not; 
+Initialized int the firmware to send at least first batch of bytes or data */
 
-unsigned short dataIndex;
-unsigned char dataSubindex;
-unsigned char sampleFlag=0;
-unsigned short batch_counter=0;
+unsigned short dataIndex;			// Used for data buffer; Limit is 750 
+unsigned char dataSubindex;			// Used for sub data buffer and has a max value of 4
+unsigned char sampleFlag=0;			// Flag to sample the ADC; gets set by the timer ISR	
+unsigned short batch_counter=0;		/* Number of bytes that need to be sent to the phone; Raw and activity
+ count data is sent in the burst mode */
 
 /* Interrupt Service Routines */
-unsigned int seconds_passed=0;
-unsigned int seconds_passed2=0;
-unsigned int counter=0;
+unsigned int seconds_passed=0;		// Counter to keep sampling accelerometer
+unsigned int seconds_passed2=0;		// Currently Not used
+unsigned int counter=0;				// Currently Not used
 
-unsigned char skip_interrupt_counter=0;
-unsigned char connected=0;
-unsigned int seconds_disconnected=0;
+unsigned char skip_interrupt_counter=0;		// Currently Not used
+unsigned char connected=0;				// To indicate if the wocket is connected to the phone
+unsigned int seconds_disconnected=0;	// To incur delay in the Timer ISR
 
-unsigned short configurationTimer=1;
+unsigned short configurationTimer=1;	// Currently Not used
 unsigned char interrupts_passed=0;
 
-unsigned char interrupt_reps=0;
-unsigned short docking_counter=0;
+unsigned char interrupt_reps=0;		/* Used to skip sampling depending on the sampling rate variables/timers
+ in the timer ISR */
+unsigned short docking_counter=0;		// Currently Not used
 
 
 /* Butterworth Filter */
 //double b[5]= {0.0051,0.0,-0.0103,0.0,0.0051};
 //double a[5]= { 1.00, -3.7856, 5.3793, -3.4016, 0.8079};
-unsigned short xv[3][41];
+unsigned short xv[3][40];		// The vector of acceleration values for one second xv[3][40];
 //double yv[3][5];
-unsigned long vmag;
-double vmags;
-unsigned short val;
-unsigned char justconnected=0;
-unsigned short blink_counter;
-unsigned char isdocked=0;
-unsigned int dockcounter=0;
-unsigned int pseq=0;
-unsigned int cc=0;
+unsigned long vmag;				// Sum of the filter from 3 axis
+double vmags;					// Currently Not used
+unsigned short val;				// Currently Not used
+unsigned char justconnected=0;	// Flag to identify when the wocket got connected
+unsigned short blink_counter;	/*// Is used in the timer2 ISR to blink green LED when connected or not
+ connected  Green will blink in either continuous (when not connected) or burt modes */
+unsigned char isdocked=0;		// Flag to indicate while charging or programming
+unsigned int dockcounter=0;		/* Counter to prevent resetting the wocket, which would lead to losing of the 
+activity count data, if someone might plug in the wocket for a short time accidentally. The counter is used to
+ count up to 2400 and then shutdown the radio and reset variables to allow for faster charging.*/
 
+unsigned int pseq=0;			// Currently Not used
+unsigned int cc=0;				// Currently Not used
 
+/* Function to filter data used for the activity count */
 unsigned short Filter(unsigned short data,int axis)
 {
 	 unsigned short mean=0;
      int j=0;           
-     for (; (j < 40); j++)
+     for (; (j < _SAMPLING_RATE); j++)	// sampling rate (j < 40)
 	 {
 	 	  mean+=xv[axis][j];
           xv[axis][j] = xv[axis][j + 1];		  		  
 	 }
-	 mean=mean/40;
+	 mean=mean/_SAMPLING_RATE;
      xv[axis][j] = data;
          
 				 
@@ -128,7 +154,8 @@ unsigned short Filter(unsigned short data,int axis)
 	 	return (mean-data);      
 }
 
-
+/*Function to perform differential compression, looks like the compression is performed 
+whenever the difference of the acceleration values on all three axes is lower than 32 */
 
 static __inline__ void _send_pdu(unsigned short x, unsigned short y, unsigned short z){	
 	if(compress)
@@ -176,24 +203,24 @@ static __inline__ void _send_pdu(unsigned short x, unsigned short y, unsigned sh
 
 
 
-
+/* Main function */
 int main()
 {
 
-
+   /* This sections checks if the wocket is docked if so it waits in a loop for 10seconds*/
 
 	if (_is_docked())
 	{
 
-		for(int j=0;(j<10);j++)
+		for(int j=0;(j<10);j++)			// 10 seconds wait
 			for(int i=0;(i<200);i++)
 				_delay_ms(5);
 	}
 				
-	scounter=0;
+	scounter=0;							// NOT BEING USED
 
 	//Initialized data buffer
-	dataIndex=0;
+	dataIndex=0;						// Data number for 1 minute: 40*60 = 2400
 	dataSubindex=0;
 
 	// Blink green for 5 seconds	
@@ -202,6 +229,8 @@ int main()
 
 	AC_NUMS=_SAMPLING_RATE *60;
 
+	/*Functions used for optimizing power by shutting down different peripherals. 
+	These functions are part of the external dependencies provided by AVR software power.h*/
 	power_adc_disable();
   	power_spi_disable();
   	power_timer0_disable();
@@ -209,13 +238,13 @@ int main()
   	power_twi_disable();
 
 
-
+	/*Loop indefinitely*/
 	while(1)
 	{
 			
 
 		//Sample only in the main loop because of p, low power mode
-		if(sampleFlag)
+		if(sampleFlag)		// Sample flag is turned ON in the timer 2 ISR (OVR)
 		{		
 		    //turn on adc and micro controller on board	
 			power_adc_enable();
@@ -249,19 +278,22 @@ int main()
 						
 				if (summary_count==0)
 				{
-					vmag=vmag/AC_SCALING;
+					vmag=vmag/AC_SCALING;	/* Scaling has been discussed during the
+					 declaration of the variable in the file firmware-version_4.c above */
 					
 					if (vmag>65535)
-						acount[ci]=65535;
+						acount[ci]=65535;	// Ceiling of the activity counts	
 					else
-						acount[ci]=(unsigned short) vmag;
+						acount[ci]=(unsigned short) vmag; /* probably the activity counts 
+						is the sum of the magnitude of the three axes */
 
 				 		vmag=0;
 						++ci;
 
 						if (ci==AC_BUFFER_SIZE)
 							ci=0;
-						cseq++;
+						cseq++;		/* if the activity counts for the first 16 hours are 
+						full then increment the c sequence	*/	
 		
 						if (ci==si)
 						{
@@ -271,12 +303,13 @@ int main()
 							sseq++;
 						}
 						acount[ci]=0;
-						summary_count=AC_NUMS;
+						summary_count=AC_NUMS;	/* 1 minute Summary counter Sampling rate*60 
+						here 2400, is associated with activity counts */
 				}
 				else
 						summary_count--;
 			}
-			else if (_wPC==40)
+			else if (_wPC==40)		// discard the first 40 samples for the vmag
 		  	{			
 				vmag=0;
 			}
@@ -291,13 +324,14 @@ int main()
 		
 
 			//Save the raw data in an optimized manner to the RAM
+			// This section of code stores the data on the RAM in an optimized manner in wocket.h 
 			 m_SET_X(data[dataIndex],x,dataSubindex);
 			 m_SET_Y(data[dataIndex],y,dataSubindex);
 			 m_SET_Z(data[dataIndex],z,dataSubindex);
 
 			 dataSubindex++;
 
-			 if (dataSubindex>=4)
+			 if (dataSubindex>=4)	// because there are 16 bytes of information
 			 	dataSubindex=0;
 		
 
@@ -341,7 +375,8 @@ int main()
 					justconnected=2;
 				}		
 
-
+				/* based on this it looks like if the firmware is in continous mode then it will 
+				not compress the data */
 				// send all the data without any compression
 				_send_uncompressed_pdu(x, y, z);
 
@@ -361,20 +396,23 @@ int main()
 			}
 			else  //if set to any of the bursty modes
 			{
-
-                // increase the batch count
+				// increase the batch count
+				// Probably used to identify the data that needs to be sent via wireless connection 
+				// BATCH_Counter < 1minute raw and activity count data 
+				                
 				if ((dataSubindex==0) && (batch_counter<750))
 					batch_counter++;
 
-
-				if (connected)
+				// Increment the second data that needs to be sent 
+				if (connected)			// check for the BT connection
 				{
 					_greenled_turn_on();
 						
 					gotack=1;
 					tester=0;
 
-					if (_wTM==_TM_Continuous)
+					if (_wTM==_TM_Continuous) /* Skips sending that particular batch data if the 
+					phone requests for a mode change */
 						continue;
                    
 
@@ -385,12 +423,19 @@ int main()
 					_receive_data();
 						
 					//-------------------
-
-
+					/* Once the wocket is connected to a phone, it immediately sends data. For some 
+					phones (devices) we lose initial packets of the data stream, transmitted by 
+					the wockets. The loss of data is probably due to some problem that
+					occurs on the receiving end (phone) during the connection setup. The current 
+					design of the wockets does not have the capability to allow flow control using 
+					RTS/CTS. Instead, we delay the transmission of the actual data (activity counts 
+					or raw data) by sending padding bytes. The decoder software on the phone ignores
+					the (0xff) bytes received by the phone.*/
 					for (int ixz=0;(ixz<100);ixz++) 
 					{                                                                                      
        					
-						_bluetooth_transmit_uart0_byte(0xff); 
+						_bluetooth_transmit_uart0_byte(0xff); /* Currently the decode on the phone
+						 ignores these packets */
 
 							
 						if (sampleFlag)
@@ -412,14 +457,15 @@ int main()
 									{
 										vmag=vmag/AC_SCALING;
 										if (vmag>65535)
-											acount[ci]=65535;
+											acount[ci]=65535;	// Ceiling if above the max value
 										else
 											acount[ci]=(unsigned short) vmag;
 			 							vmag=0;
 										++ci;
 										if (ci==AC_BUFFER_SIZE)
 											ci=0;
-										cseq++;
+										cseq++;		/* if the activity counts for the first 16 
+										hours are full then increment the c sequence	*/	
 	
 										if (ci==si)
 										{
@@ -429,16 +475,15 @@ int main()
 											sseq++;
 										}
 										acount[ci]=0;
-										summary_count=AC_NUMS;
+										summary_count=AC_NUMS; /* // 1 minute Summary counter 
+										Samplingrate*60 = 2400, is associated with activity counts*/
 									}else
-										summary_count--;
+										summary_count--; // Discard the first 40 samples for the vmag
 								}
 								else if (_wPC==40)
 									vmag=0;
-
-			
-
-		
+				
+								// storing data on the memory 
 			 					m_SET_X(data[dataIndex],x,dataSubindex);
 			 					m_SET_Y(data[dataIndex],y,dataSubindex);
 			 					m_SET_Z(data[dataIndex],z,dataSubindex);
@@ -454,18 +499,19 @@ int main()
 				// Test Code ----------------
 					//_send_fv();
 					//_send_hv();					
-					_send_sr();					 
-					_send_tm();	
+					_send_sr();			// Send the sampling rate to the phone 				 
+					_send_tm();			// Send the transmission mode to the phone
 						
 
 					//sample and send the battery level
 					battery =_atmega_a2dConvert10bit(ADC7); 
-					_send_bl(battery);
+					_send_bl(battery);	// Send the battery level to the phone	
 				//----------------------------
 
 					//send activity counts information
-					_send_batch_count((batch_counter-1)*4);																	
-					_send_acs();
+					_send_batch_count((batch_counter-1)*4);	/* Send the batch count 
+					will send number of activity counts in the batch to the  phone*/																
+					_send_acs();		// Send the Activity counts to the phone
 
 
 					//Send summary activity count
@@ -482,7 +528,8 @@ int main()
 					//Send raw data
 					if ((batch_counter>0) && (batch_counter<750)) // Go from 0 up to batch_counter
 					{						
-						for (int i=0;(i<(batch_counter-1));i++)
+						// Upto a minute and a half of raw data is sent 
+						for (int i=0;(i<(batch_counter-1));i++)		
 						{
 							m_GET_X(x,data[i].byte1,data[i].byte2,0);
 							m_GET_Y(y,data[i].byte2,data[i].byte3,0);
@@ -514,7 +561,6 @@ int main()
 							_send_pdu(x,y,z);
 
 							_receive_data();
-
 
 							if (sampleFlag)
 							{
@@ -555,7 +601,7 @@ int main()
 										summary_count=AC_NUMS;
 									}else
 										summary_count--;
-								}
+								}	//End of if (_wPC>40)
 								else if (_wPC==40)
 									vmag=0;
 
@@ -568,8 +614,8 @@ int main()
 			 					dataSubindex++;
 			 					if (dataSubindex>=4)
 			 						dataSubindex=0;
-							}
-						}
+							}	//End of if(sample_flag)
+						}		// End of for (int i=0;(i<(batch_counter-1));i++)	
 
 						
 						if (batch_counter>0)
@@ -594,7 +640,7 @@ int main()
 
 
 					}
-					else
+					else		// Crcular buffer sending last bytes of data
 					{
 
 						int current=dataIndex+1;
@@ -821,11 +867,12 @@ int main()
 			cli();
 			set_sleep_mode(SLEEP_MODE_IDLE);
 			//set_sleep_mode(SLEEP_MODE_PWR_SAVE);
-    		sleep_enable();
-    		sleep_bod_disable(); 	
-    		sei();
-    		sleep_cpu();
-    		sleep_disable();
+			/// Built in functionality to enable interrupts and shutdown of the cpu to save power 
+    		sleep_enable();		// sleep.h 
+    		sleep_bod_disable(); 	// sleep.h 
+    		sei();		//interrupt.h
+    		sleep_cpu();		// sleep.h 
+    		sleep_disable();	// sleep.h 	
 
 
 	}
@@ -835,7 +882,7 @@ int main()
 
 
 
-
+/* Interrupt service routine for Timer 2*/
 
 
 ISR(TIMER2_OVF_vect)
@@ -851,7 +898,7 @@ ISR(TIMER2_OVF_vect)
 
 	if (_is_docked())
 	{
-		dockcounter++;		
+		dockcounter++;		// 2400/40 = 1 minute of docking at least 
 		if ((!isdocked)&& (dockcounter>2400)){	
 		
 				
@@ -866,7 +913,7 @@ ISR(TIMER2_OVF_vect)
 	}else
 	{
 		dockcounter=0;
-		if (isdocked)
+		if (isdocked)	// Probably isdocked is a FLAG which is reset in this routine
 		{
 			_bluetooth_turn_on();
 			isdocked=0;			
@@ -875,9 +922,9 @@ ISR(TIMER2_OVF_vect)
 
 	if (connected==0){
 		blink_counter++;
-		if (blink_counter==(_SAMPLING_RATE*5))
+		if (blink_counter==(_SAMPLING_RATE*5))		// ON period: 40*5 =200
 			_greenled_turn_on();
-		else if (blink_counter==((_SAMPLING_RATE*5)+10))
+		else if (blink_counter==((_SAMPLING_RATE*5)+10)) 	// OFF period: 40*5 +10 =200 +10
 		{
 			_greenled_turn_off();
 			blink_counter=0;
@@ -918,6 +965,8 @@ ISR(TIMER2_OVF_vect)
 
 
 	/* Skip sampling depending on the sampling rate variables/timers */
+	/*REFER _wocket_initialize_timer2_interrupt as the sampling hass to 
+	be much slower than the MCU can actually sample ( 40HZ) */
  	if (interrupt_reps==0)
 	{	
 		interrupt_reps=_wTCNT2_reps;
@@ -940,7 +989,7 @@ ISR(TIMER2_OVF_vect)
 	{
 
 		_wPC++;
-
+		// Section of the code to indicate that the wocket got connected
 		if (!_bluetooth_is_connected())
 		{
 			justconnected=0;
@@ -978,7 +1027,8 @@ ISR(TIMER2_OVF_vect)
 
 		//Increase the packet counter
 		 _wPC++;
-
+/* Segment of the code that turns the Bluetooth ON every 45 seconds approximately 
+after a previous transmission to have the wocket ready for a connection. */
 
 		if (!_bluetooth_is_connected())
 		{
