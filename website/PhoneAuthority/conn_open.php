d50f598e58b959ceac8fefc6303fef24128e47c4
mit.boxlab <mit.boxlab@gmail.com>
2011-12-02 20:26:19 +0000

    Removed Old (WM6) version of the website, which was significantly out of date.

--- website/PhoneAuthority/conn_open.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	$mysql_server_name='wockets.media.mit.edu';
-	$mysql_username='wockets';
-	$mysql_password='76giotto';
-	$mysql_database='phoneauthority';
-	$conn=mysql_connect($mysql_server_name,$mysql_username,$mysql_password,$mysql_database); 
-	mysql_select_db($mysql_database,$conn); 
-?>
