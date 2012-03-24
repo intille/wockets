82b770777ce348413ce73ea5615421ea93c283fa
mit.boxlab@gmail.com <mit.boxlab@gmail.com@1b6154ff-f548-0410-bdff-7fc7a0f7d33b>
2012-03-24 23:13:16 +0000

    Revised to version which handles zip of json

--- trunk/server/FileUploader/src/Resources/ServiceClient.java
+++ trunk/server/FileUploader/src/Resources/ServiceClient.java
@@ -3,6 +3,11 @@ package Resources;
 import java.io.IOException;
 
 import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.URI;
+import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.StringRequestEntity;
 
@@ -21,7 +26,7 @@ public class ServiceClient {
 	        PostMethod filePost = new PostMethod(POST_URL);
 	        
 	        
-	        filePost.setRequestHeader("Content-type", "application/x-json; charset=UTF-8");
+	        filePost.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	        filePost.setRequestEntity(new StringRequestEntity(jsonString));
 	        
 	        int status = client.executeMethod(filePost);
@@ -33,4 +38,45 @@ public class ServiceClient {
 	        
 	        return status;
 	    }
+	
+	public static String getSubjectID(String phoneID) {
+
+		String subjectID = "";
+	    HttpClient client = new HttpClient();
+	    client.getParams().setParameter("http.useragent", "Test Client");
+	    client.getParams().setParameter("http.connection.timeout",new Integer(5000));
+
+	    GetMethod method  = new GetMethod();
+	    
+	    try {
+
+	      method.setURI(new URI("http://wockets.ccs.neu.edu:8080/Wockets/android/getParticipantId.html", true));
+	      
+	      NameValuePair imei = new NameValuePair();
+	      imei.setName("imei");
+	      imei.setValue(phoneID);
+	      
+	      NameValuePair[] params = new NameValuePair[] {imei};
+	      
+	      method.setQueryString(params);
+	      
+	      
+	      int returnCode = client.executeMethod(method);
+
+	      
+	      if(returnCode == HttpStatus.SC_OK) {
+	        subjectID = method.getResponseBodyAsString();
+	      }else if(method.getResponseBodyAsString().contains("No record found for imei:"+phoneID))
+	    	  subjectID = "NoRecordFound";
+	      
+	    } catch (HttpException he) {
+	      System.err.println(he);
+	    } catch (IOException ie) {
+	      System.err.println(ie);
+	    } finally {
+	      method.releaseConnection();
+	    }
+	    return subjectID;
+
+	  }
 }
